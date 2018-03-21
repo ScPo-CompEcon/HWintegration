69ebc96323dd27b0a4adba38cd73169b717b5fe3
Vladimir Avetyan <vl.avetyan@gmail.com>
2018-03-21 16:23:46 +0100

    Second  task (partly)

--- src/HWintegration.jl
+++ src/HWintegration.jl
@@ -29,6 +29,8 @@ module HWintegration
 
 	# eqm condition for question 2
 
+	egm_cond(θ1,θ2,p) = exp(θ1).*p^(-1) .+ exp(θ2).*p^(-0.5) - 2
+
 	# makes a plot for questions 1
 	function plot_q1()
 		plot(p->q(p),0.5,5,label="Q")
@@ -78,9 +80,17 @@ module HWintegration
 	# question 2
 
 	function question_2a(n)
-		repeat(gausshermite(n)[1], inner=[1], outer=[n]) 
-		
-
+		nodes, weights = gausshermite(n)
+		mat_cov = hcat([0.02, 0.01],[0.01,0.01])
+		nodes_kron = hcat(kron(ones(n),nodes),kron(nodes,ones(n)))
+		weights_kron = kron(weights,weights)
+		nodes_adj = chol(mat_cov)*nodes_kron'
+		nodes_res = Vector{Float64}(n*n)
+		for i in 1:(n*n)
+			nodes_res[i] = fzero(p->egm_cond(nodes_adj[1,i],nodes_adj[2,i],p),1.0)
+		end
+		res = dot(weights_kron,nodes_res)
+		return res
 	end
 
 	function question_2b(n)
@@ -110,9 +120,11 @@ module HWintegration
 			info("question 1d:")
 			question_1d(n)
 			println("")
-			#info("question 2a:")
-			#q2 = question_2a(n)
-			#println(q2)
+			info("question 2a:")
+			if n == 10
+				q2 = question_2a(n)
+				println(q2)
+			end
 			#info("question 2b:")
 			#q2b = question_2b(n)
 			#println(q2b)
@@ -121,6 +133,7 @@ module HWintegration
 			#println(q2bo)
 		end
 	end
+	runall()
 	info("end of HWintegration")
 
 end
